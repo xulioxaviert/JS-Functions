d1c8579e8bcf56dca26bfbb0c546defad78ecca5
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-03-01 16:11:26 +0100

    Iteración #4: Calcular el promedio

--- average.js
+++ average.js
@@ -4,9 +4,20 @@
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
 
-
+let avge = 0;
 function average(list) {
-    let sum = 0;
-    let iteam = 0;
+  let sum = 0;
+  let item = list.length;
+  
 
+  for (let i = 0; i < list.length; i++) {
+    sum = list[i] + sum;
+  }
+
+  avge = sum / item;
+  
 }
+
+
+average(numbers);
+console.log(avge);
